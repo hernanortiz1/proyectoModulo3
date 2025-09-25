1181ea5ba5116a329873dee7184174d399d696d2
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-09-25 12:54:17 -0300

    agregue en la ruta del index la ruta a los productos

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,1 +1,8 @@
-//routes
+import { Router } from "express";
+import productoRoutes from "./productos.routes.js";
+
+const router = Router()
+
+router.use("/productos", productoRoutes)
+
+export default router;
