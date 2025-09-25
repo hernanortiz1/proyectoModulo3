23494795c8d143646337584b046f4917bb039f1a
Nacho <barrojojoaquin2@gmail.com>
2025-09-25 10:42:54 -0300

    agregue los controladores para crear, editar, leer y borrar los productos

---
+++ src/controllers/producto.controllers.js
@@ -0,0 +1,56 @@
+import Producto from "../models/producto.models";
+
+export const crearProducto = async (req, res) => {
+    try {
+        const nuevoProducto = new Producto(req.body);
+        await nuevoProducto.save();
+        res.status(201).json(nuevoProducto);
+    } catch (error) {
+        res.status(500).json({ error: "Error al crear el producto" });
+    }
+}
+
+export const obtenerProductos = async (req, res) => {
+    try {
+        const productos = await Producto.find();
+        res.status(200).json(productos);
+    } catch (error) {
+        res.status(500).json({ error: "Error al obtener los productos" });
+    }
+}
+
+export const obtenerProductoPorId = async (req, res) => {
+    try {
+        const buscarProducto = await Producto.findById(req.params.id);
+        if (!buscarProducto) {
+            return res.status(404).json({ error: "Producto no encontrado" });
+        }
+        res.status(200).json(buscarProducto);
+    } catch (error) {
+        res.status(500).json({ error: "Error al obtener el producto" });
+    }
+}
+
+export const actualizarProducto = async (req, res) => {
+    try {
+        const productoActualizado = await Producto.findByIdAndUpdate(req.params.id, req.body);
+        if (!productoActualizado) {
+            return res.status(404).json({ error: "Producto no encontrado" });
+        }
+        res.status(200).json(productoActualizado);
+    } catch (error) {
+        res.status(500).json({ error: "Error al actualizar el producto" });
+    }
+}
+
+export const eliminarProducto = async (req, res) => {
+    try {
+        const productoEliminado = await Producto.findByIdAndDelete(req.params.id);
+        if(!productoEliminado){
+            return res.status(404).json({ error: "Producto no encontrado" });
+        }
+        res.status(200).json({ message: "Producto eliminado" });
+    } catch (error) {
+        res.status(500).json({ error: "Error al eliminar el producto" });
+    }
+}
