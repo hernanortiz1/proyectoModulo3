b5b5ef79d73a3656b0235a0f57bff02f27c46823
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-09-26 09:59:22 -0300

    agregue ruta a index

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import productoRoutes from "./productos.routes.js";
+import usuarioRoutes from "./usuario.routes.js"
 
 const router = Router()
 
 router.use("/productos", productoRoutes)
+
+router.use("/usuarios", usuarioRoutes)
 
 export default router;
