b35dc61704e46aee39ea405cfe95d11b29eb831f
Lucasfi <lukafigueroa64@gmail.com>
2025-09-26 11:34:35 -0300

    Agregamos validacion en el campo stock, talle y color

--- src/middelware/validarProducto.js
+++ src/middelware/validarProducto.js
@@ -51,6 +51,42 @@ const validacionProducto = [
     .withMessage(
       "La categoria debe ser una de las siguientes opciones: Remeras y chombas, Abrigos y camperas, Sweaters y buzos, Camisas, Bermudas, Pantalones, Shorts de Baño, Anteojos de sol, Gorras"
     ),
+  body("stock")
+    .notEmpty()
+    .withMessage("Stock es un dato obligatorio")
+    .isLength({ min: 1, max: 5000 })
+    .withMessage("El stock debe puede tener entre 1 y 500 productos"),
+  body("descripcion")
+    .notEmpty()
+    .withMessage("La descripcion es obligatoria")
+    .isLength({ min: 10, max: 500 })
+    .withMessage("La descripcion debe tener entre 10 y 500 caracteres"),
+  body("fechaUltimoControlStock")
+    .notEmpty()
+    .withMessage("La fecha de control es obligatoria")
+    .isISO8601()
+    .withMessage("La fecha debe tener un formato válido")
+    .custom((value) => {
+      const fecha = new Date(value);
+      if (fecha > new Date()) {
+        throw new Error("La fecha de control no puede ser futura");
+      }
+      return true;
+    }),
+  body("talle")
+    .notEmpty()
+    .withMessage("El talle es un dato obligatorio")
+    .isIn(["XS", "S", "M", "L", "XL", "XXL", "XXXL", "Único"])
+    .withMessage(
+      "El talle debe ser una del as siguientes opciones: XS, S, M, L, XL, XXL, XXXL, Unico"
+    ),
+  body("color")
+    .notEmpty()
+    .withMessage("El color es obligatorio")
+    .isLength({ min: 3, max: 20 })
+    .withMessage("El color debe tener al menos 3 caracteres")
+    .isAlpha()
+    .withMessage("El color debe contener solo letras"),
 
   (req, res, next) => resultadoValidacion(req, res, next),
 ];
