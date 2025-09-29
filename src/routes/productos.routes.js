5705c29aa3b65643cab700b87b9d6af63884fe62
Lucasfi <lukafigueroa64@gmail.com>
2025-09-29 10:26:29 -0300

    Agregue errorMulter y upload solo para campo imagen en ruta post en producto.routes.js

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -9,13 +9,15 @@ import {
 } from "../controllers/producto.controllers.js";
 import validacionProducto from "../middlewares/validarProducto.js";
 import verificarToken from "../middlewares/verificarToken.js";
+import upload from "../middlewares/upload.js";
+import errorMulter from "../middlewares/errorMulter.js";
 
 const router = Router();
 
 router
   .route("/")
   .get(obtenerProductos)
-  .post([verificarToken, validacionProducto], crearProducto);
+  .post([verificarToken, upload.single('imagen'), errorMulter, validacionProducto], crearProducto);
   router.route("/paginacion").get(productosPaginados)
 router
   .route("/:id")
