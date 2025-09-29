05ec35802c4e1418e7a72b99798b2ac0ea3f1952
Lucasfi <lukafigueroa64@gmail.com>
2025-09-29 10:20:41 -0300

    Use uploader de cloduinary y cree nombre de carpetas productos donde guardamos las imagenes

--- src/helpers/cloudinaryUploader.js
+++ src/helpers/cloudinaryUploader.js
@@ -2,7 +2,18 @@ import cloudinary from "./cloudinary";
 
 const subirImagenACloudinary = (buffer) => {
   return new Promise((resolve, reject) => {
-    
+
+    const stream = cloudinary.uploader.upload_stream(
+      { folder: "productos" },
+      (error, result) => {
+        if(result) {
+          resolve(result);
+        } else {
+          reject(error);
+        }
+      }
+    );
+    stream.end(buffer)
   });
 };
 
