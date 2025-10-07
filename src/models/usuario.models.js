0b31a448f95573188b5f21d27900b7531e2b7c20
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-10-07 16:44:59 -0300

    quite caracterisiticas del passwrd en su modelo

--- src/models/usuario.models.js
+++ src/models/usuario.models.js
@@ -22,16 +22,8 @@ const usuarioSchema = new Schema(
     },
     password: {
       type: String,
-      required: true,
       minLength: 8,
       maxLength: 100,
-      validate: {
-        validator: (valor) => {
-          return /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,100}$/.test(
-            valor
-          );
-        },
-      },
     },
     rol: {
       type: String,
