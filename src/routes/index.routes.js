fd0959b6e55ac417aec6154b3621f6ec1505d220
Lucasfi <lukafigueroa64@gmail.com>
2025-10-29 15:52:47 -0300

    Agregue ruta de pagos en index

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import productoRoutes from "./productos.routes.js";
 import usuarioRoutes from "./usuario.routes.js"
+import pagosRoutes from "./pagos.routes.js"
 
 const router = Router()
 
 router.use("/productos", productoRoutes)
 
 router.use("/usuarios", usuarioRoutes)
+
+router.use("/pagos", pagosRoutes)
 
 export default router;
